c08d4059b7cd0e797546ff678ec4386f494f5982
adriano-repetti <adriano.repetti@arm.com>
2019-12-02 16:41:51 +0000

    Doc

--- packages/core/src/common/command.ts
+++ packages/core/src/common/command.ts
@@ -151,7 +151,7 @@ export interface CommandService {
     readonly onWillExecuteCommand: Event<WillExecuteCommandEvent>;
 
     /**
-     * An event is emmited when a command has been successfully to be executed.
+     * An event is emmited when a command has been successfully executed.
      */
     readonly onDidExecuteCommand: Event<DidExecuteCommandEvent>;
 }
