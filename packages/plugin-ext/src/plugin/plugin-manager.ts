38f6f4b9bc2d333f3c27b1340a3d24c2125f1224
Rob Moran <rob.moran@arm.com>
2020-06-28 21:29:41 +0100

    handle plugin host activation exceptions
    Signed-off-by: Rob Moran <rob.moran@arm.com>

--- packages/plugin-ext/src/plugin/plugin-manager.ts
+++ packages/plugin-ext/src/plugin/plugin-manager.ts
@@ -220,31 +220,35 @@ export class PluginManagerExtImpl implements PluginManagerExt, PluginManager {
         let loading = this.loadedPlugins.get(plugin.model.id);
         if (!loading) {
             loading = (async () => {
-                if (plugin.rawModel.extensionDependencies) {
-                    for (const dependencyId of plugin.rawModel.extensionDependencies) {
-                        const dependency = this.registry.get(dependencyId.toLowerCase());
-                        const id = plugin.model.displayName || plugin.model.id;
-                        if (dependency) {
-                            const depId = dependency.model.displayName || dependency.model.id;
-                            const loadedSuccessfully = await this.loadPlugin(dependency, configStorage, visited);
-                            if (!loadedSuccessfully) {
-                                const message = `Cannot activate extension '${id}' because it depends on extension '${depId}', which failed to activate.`;
-                                this.messageRegistryProxy.$showMessage(MainMessageType.Error, message, {}, []);
-                                return false;
+                try {
+                    if (plugin.rawModel.extensionDependencies) {
+                        for (const dependencyId of plugin.rawModel.extensionDependencies) {
+                            const dependency = this.registry.get(dependencyId.toLowerCase());
+                            if (dependency) {
+                                const loadedSuccessfully = await this.loadPlugin(dependency, configStorage, visited);
+                                if (!loadedSuccessfully) {
+                                    throw new Error(`Dependent extension '${dependency.model.displayName || dependency.model.id}' failed to activate.`);
+                                }
+                            } else {
+                                throw new Error(`Dependent extension '${dependencyId}' is not installed.`);
                             }
-                        } else {
-                            const message = `Cannot activate the '${id}' extension because it depends on the '${dependencyId}' extension, which is not installed.`;
-                            this.messageRegistryProxy.$showMessage(MainMessageType.Error, message, {}, []);
-                            console.warn(message);
-                            return false;
                         }
                     }
-                }
 
-                let pluginMain = this.host.loadPlugin(plugin);
-                // see https://github.com/TypeFox/vscode/blob/70b8db24a37fafc77247de7f7cb5bb0195120ed0/src/vs/workbench/api/common/extHostExtensionService.ts#L372-L376
-                pluginMain = pluginMain || {};
-                return this.startPlugin(plugin, configStorage, pluginMain);
+                    let pluginMain = this.host.loadPlugin(plugin);
+                    // see https://github.com/TypeFox/vscode/blob/70b8db24a37fafc77247de7f7cb5bb0195120ed0/src/vs/workbench/api/common/extHostExtensionService.ts#L372-L376
+                    pluginMain = pluginMain || {};
+                    await this.startPlugin(plugin, configStorage, pluginMain);
+                    return true;
+                } catch (err) {
+                    if (this.pluginActivationPromises.has(plugin.model.id)) {
+                        this.pluginActivationPromises.get(plugin.model.id)!.reject(err);
+                    }
+                    const message = `Activating extension '${plugin.model.displayName || plugin.model.name}' failed: ${err.message}`;
+                    this.messageRegistryProxy.$showMessage(MainMessageType.Error, message, {}, []);
+                    console.error(message);
+                    return false;
+                }
             })();
         }
         this.loadedPlugins.set(plugin.model.id, loading);
@@ -269,7 +273,7 @@ export class PluginManagerExtImpl implements PluginManagerExt, PluginManager {
     }
 
     // tslint:disable-next-line:no-any
-    private async startPlugin(plugin: Plugin, configStorage: ConfigStorage, pluginMain: any): Promise<boolean> {
+    private async startPlugin(plugin: Plugin, configStorage: ConfigStorage, pluginMain: any): Promise<void> {
         const subscriptions: theia.Disposable[] = [];
         const asAbsolutePath = (relativePath: string): string => join(plugin.pluginFolder, relativePath);
         const logPath = join(configStorage.hostLogPath, plugin.model.id); // todo check format
@@ -298,29 +302,19 @@ export class PluginManagerExtImpl implements PluginManagerExt, PluginManager {
         }
         const id = plugin.model.displayName || plugin.model.id;
         if (typeof pluginMain[plugin.lifecycle.startMethod] === 'function') {
-            try {
-                const pluginExport = await pluginMain[plugin.lifecycle.startMethod].apply(getGlobal(), [pluginContext]);
-                this.activatedPlugins.set(plugin.model.id, new ActivatedPlugin(pluginContext, pluginExport, stopFn));
+            const pluginExport = await pluginMain[plugin.lifecycle.startMethod].apply(getGlobal(), [pluginContext]);
+            this.activatedPlugins.set(plugin.model.id, new ActivatedPlugin(pluginContext, pluginExport, stopFn));
 
-                // resolve activation promise
-                if (this.pluginActivationPromises.has(plugin.model.id)) {
-                    this.pluginActivationPromises.get(plugin.model.id)!.resolve();
-                    this.pluginActivationPromises.delete(plugin.model.id);
-                }
-            } catch (err) {
-                if (this.pluginActivationPromises.has(plugin.model.id)) {
-                    this.pluginActivationPromises.get(plugin.model.id)!.reject(err);
-                }
-                this.messageRegistryProxy.$showMessage(MainMessageType.Error, `Activating extension ${id} failed: ${err.message}.`, {}, []);
-                console.error(`Error on activation of ${plugin.model.name}`, err);
-                return false;
+            // resolve activation promise
+            if (this.pluginActivationPromises.has(plugin.model.id)) {
+                this.pluginActivationPromises.get(plugin.model.id)!.resolve();
+                this.pluginActivationPromises.delete(plugin.model.id);
             }
         } else {
             // https://github.com/TypeFox/vscode/blob/70b8db24a37fafc77247de7f7cb5bb0195120ed0/src/vs/workbench/api/common/extHostExtensionService.ts#L400-L401
             console.log(`plugin ${id}, ${plugin.lifecycle.startMethod} method is undefined so the module is the extension's exports`);
             this.activatedPlugins.set(plugin.model.id, new ActivatedPlugin(pluginContext, pluginMain));
         }
-        return true;
     }
 
     getAllPlugins(): Plugin[] {
